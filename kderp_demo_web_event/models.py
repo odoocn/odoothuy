b292306e561d8ad09ac99da44e848c8e7bad43bb
dautienthuy <dautienthuy@gmail.com>
2015-06-24 15:41:09 +0700

    [IMP]Them style

--- kderp_demo_web_event/models.py
+++ kderp_demo_web_event/models.py
@@ -1,4 +1,6 @@
 from openerp.osv import fields, osv
+import time
+
 class kderp_demo_web_event(osv.osv):
     _name = "kderp.demo.web.event"
     _order = "id desc"
@@ -6,6 +8,6 @@ class kderp_demo_web_event(osv.osv):
     _columns = {
                'code' : fields.char("Code"),
                'name' : fields.char("Name"),
-               'date' : fields.date("Date")
+               'date' : fields.date("Date"),
                 }
 kderp_demo_web_event()
