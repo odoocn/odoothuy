c0081ae8a60c204ccd36c20a3bd5d7fcc7462ade
dautienthuy <dautienthuy@gmail.com>
2015-06-29 16:38:45 +0700

    [ADD]Them module demo_extend_web

---
+++ demo_extend_web/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Demo Extend Web",
+    'summary': """Demo Extend Webt""",
+    'description': """
+            This is module Extend Web
+            """,
+    'author': "Team IT Kinden",
+    'website': "http://www.kinden.com.vn",
+    'category': 'Kderp Demo Web Event',
+    'version': '0.1',
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'board', 'website'],
+    # always loaded
+    'data': [ 
+             'templates.xml',
+    ],
+    'demo':[
+           ]
+}
