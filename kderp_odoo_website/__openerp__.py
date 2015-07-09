a9e050f8f3575106d2bf029a693b0723f8b21747
dautienthuy <dautienthuy@gmail.com>
2015-07-09 10:16:29 +0700

    [IMP]ke thua module blog

---
+++ kderp_odoo_website/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Kderp Odoo Website",
+    'summary': """Kderp Odoo Website""",
+    'description': """
+            This is module Kderp Odoo Website
+            """,
+    'author': "Team IT Kinden",
+    'website': "http://www.kinden.com.vn",
+    'category': 'Kderp Odoo Website',
+    'version': '0.1',
+    # any module necessary for this one to work correctly
+    'depends': ['website','website_partner', 'website_blog'],
+    # always loaded
+    'data': [ 
+             'views/blog_post_views.xml',
+    ],
+    'demo':[
+           ]
+}
---
+++ kderp_odoo_website/models.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from openerp import models, fields, api
+# Storing data
+class odoo_website(models.Model):
+    _inherit = 'blog.post'
+    _name = 'blog.post'
