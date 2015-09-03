32ea6cf33f229a292b41ffb374433fbcbb3e053f
dautienthuy <dautienthuy@gmail.com>
2015-09-03 16:07:15 +0700

    [ADD]Theme demo cho odoo

---
+++ dummy_theme/__openerp__.py
@@ -0,0 +1,22 @@
+{
+    # Theme information
+    'name': "Dummy theme",
+    'description': """
+        Dummy Theme
+    """,
+    'category': 'Website',
+    'version': '1.0',
+    'depends': ['website'],
+
+    # templates, pages, and snippets
+    'data': [
+        'views/pages.xml',
+        'views/options.xml',
+        'views/snippets.xml',
+    ],
+
+    # Your information
+    'author': "OPDevel",
+    'website': "www.opdevel.com",
+    'application': True,
+}
