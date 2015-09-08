70c491291450b44da8b8e675e98510e4310049fd
dautienthuy <dautienthuy@gmail.com>
2015-09-08 15:15:01 +0700

    [IMP]ke thu page blog thay doi mau nen cua blog

---
+++ demo_blog_theme/__openerp__.py
@@ -0,0 +1,21 @@
+{
+    #Theme infomation
+    'name':'Demo Blog Theme',
+    'description':"""
+        Thay Theme trong Blog
+    """,
+    'category':'Website',
+    'version':'1.0',
+    'depends':['website','website_blog', 'sale'],
+    
+    #tempalte, pages, and snippets
+    'data':[
+            'views/pages.xml',
+            'views/assets.xml',
+            ],
+    
+    #Information
+    'author': "IT_Kinden",
+    'website':"www.kinden.com.vn",
+    'application':True,
+}
