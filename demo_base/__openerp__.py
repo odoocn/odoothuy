7a13dc895d497a88f07b39d28172cce2bb855268
dautienthuy <dautienthuy@gmail.com>
2015-09-09 16:23:27 +0700

    [ADD]Them blog vao company

---
+++ demo_base/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Demo Base Web",
+    'summary': "Demo Base Web Simple ",
+    'description':"Day la module demo Homepage",
+    'author':"IT KDVN",
+    'website':"",
+    'category':"Base Demo Web",
+    'version':"0.1",
+    'depends':['base','website_blog'],
+    'data':[
+            'kderp_company_view.xml'
+            ],
+    'demo':[],
+}
---
+++ demo_base/kderp_company.py
@@ -0,0 +1,39 @@
+import re
+import time
+
+import openerp
+from openerp import tools
+from openerp.osv import fields, osv
+from openerp.tools import float_round, float_is_zero, float_compare
+from openerp.tools.translate import _
+    
+class res_company(osv.osv):
+    _inherit = 'res.company'
+    _name = 'res.company'
+    
+    
+    _columns={
+            'office_ids': fields.one2many('kderp.demo.web.office','company_id', 'Office'),
+            'blog_ids': fields.one2many('blog.blog','company_id', 'Blog'),
+             }
+    
+    
+res_company()
+
+class kderp_demo_web_office(osv.osv):
+    _name = 'kderp.demo.web.office'
+    _columns = {    
+        'company_id': fields.many2one('res.company', 'Company'), 
+        'code' : fields.char("Code", translate=True),
+        'name' : fields.char("Name", translate=True),
+        'address' : fields.char("Address", translate=True),
+        'tel' : fields.char("Tel", translate=True),
+        'fax' : fields.char("Fax", translate=True)
+    }
+    
+class blog_blog(osv.osv):
+    _inherit = 'blog.blog'
+    _name = 'blog.blog'
+    _columns={
+            'company_id': fields.many2one('res.company', string="Company"),
+              }
