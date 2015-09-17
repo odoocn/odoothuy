8d860eaacfc0c3961016730bd42b75e4c19be517
dautienthuy <dautienthuy@gmail.com>
2015-09-17 14:41:15 +0700

    [ADD]Hien thi du lieu ti blog ra homepage

---
+++ demo_homepage/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Demo Homepage",
+    'summary': """Demo Homepage Web""",
+    'description': """
+            Day la trang web Homepage ke thua Homepage odoo
+            """,
+    'author': "Team IT Kinden",
+    'website': "http://www.kinden.com.vn",
+    'category': 'Demo Homepage',
+    'version': '0.1',
+    # any module necessary for this one to work correctly
+    'depends': ['demo_base'],
+    # always loaded
+    'data': [ 
+             'templates.xml',
+    ],
+    'demo':[
+           ]
+}
