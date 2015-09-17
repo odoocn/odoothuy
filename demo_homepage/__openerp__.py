4fa1b67ab14c1dad824b666ba27226752a8dda57
dautienthuy <dautienthuy@gmail.com>
2015-09-17 16:33:40 +0700

    [FIX]them contact vao footer

--- demo_homepage/__openerp__.py
+++ demo_homepage/__openerp__.py
@@ -10,7 +10,7 @@
     'category': 'Demo Homepage',
     'version': '0.1',
     # any module necessary for this one to work correctly
-    'depends': ['demo_base'],
+    'depends': ['demo_base', 'website_partner'],
     # always loaded
     'data': [ 
              'templates.xml',
