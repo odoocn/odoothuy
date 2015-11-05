4a1cf8e55ff788064cb2931dc34b0305917e71f5
dautienthuy <dautienthuy@gmail.com>
2015-11-05 16:25:41 +0700

    [ADD]Them module thuy demo

---
+++ kderp_website_theme_thuy_demo/__openerp__.py
@@ -0,0 +1,23 @@
+{
+    #Theme infomation
+    'name':'Kderp Website Theme Thuy Demo',
+    'description':"""
+        Kderp Website Theme Thuy Demo
+    """,
+    'category':'Website',
+    'version':'1.0',
+    'depends':['website','website_blog', 'sale'],
+    
+    #tempalte, pages, and snippets
+    'data':[
+            'views/layout.xml',
+            'views/pages.xml',
+            'views/data.xml',
+            'views/assets.xml',
+            ],
+    
+    #Information
+    'author': "IT_Kinden",
+    'website':"www.kinden.com.vn",
+    'application':True,
+}
